6d6071dcc01f821bc14d43af48d26d552d56fe24
Luis Pedraza <luispedraza@gmail.com>
2012-11-28 18:45:43 +0100

    mejoras estéticas adicionales en la pantalla de pruebas

--- static/js/test.js
+++ static/js/test.js
@@ -3,12 +3,14 @@ window.addEventListener("load", initCalendarPagination, false);
 currentCalendar = 0
 totalCalendars = 0
 solutions = []
+numbers = []
 function initCalendarPagination() {
 	solutions = document.getElementsByClassName("calendar")
+	numbers = document.getElementsByClassName("bnumber")
 	if (solutions.length) {
 		totalCalendars = solutions.length
 		document.getElementById("control").style.display="block"
-		solutions[0].style.display="block"
+		selectCalendar(0)
 		document.getElementById("prev-cal").addEventListener("click",
 			function(e) {
 				changeCalendar(e,-1)
@@ -17,15 +19,27 @@ function initCalendarPagination() {
 			function(e) {
 				changeCalendar(e,1)
 			})
+		for (n=0; n<numbers.length; n++) {
+			numbers[n].addEventListener("click", selectCalendar)
+		}
 	}
 }
 
-function changeCalendar(event, inc) {
+function changeCalendar(e, inc) {
+	calendar = currentCalendar + inc
+	if (calendar==totalCalendars)
+		calendar = 0
+	else if (calendar<0)
+		calendar=totalCalendars-1
+	selectCalendar(calendar)
+}
+
+function selectCalendar(number) {
+	if (typeof number != 'number')
+		number = parseInt(this.innerHTML)
 	solutions[currentCalendar].style.display = "none"
-	currentCalendar += inc
-	if (currentCalendar==totalCalendars)
-		currentCalendar = 0
-	else if (currentCalendar<0)
-		currentCalendar=totalCalendars-1
+	numbers[currentCalendar].style.fontSize = "1em"
+	currentCalendar = number
 	solutions[currentCalendar].style.display = "block"
+	numbers[currentCalendar].style.fontSize = "1.5em"
 }
